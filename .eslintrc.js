9b935e561b574385480d4b8a67c86b228a6a7bc5
yuri2peter <yuri2peter@qq.com>
2019-06-25 19:31:01 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    "es6": true,
+  },
+  extends: 'standard',
+  plugins: [
+    'html'
+  ],
+  'rules': {
+    'semi': [2, "always"],
+    "generator-star-spacing": [0],
+    "consistent-return": [0],
+    "import/prefer-default-export": [0],
+    "no-else-return": [0],
+    "no-restricted-syntax": [0],
+    "import/no-extraneous-dependencies": [0],
+    "no-use-before-define": [0],
+    "no-nested-ternary": [0],
+    "arrow-body-style": [0],
+    "import/extensions": [0],
+    "no-bitwise": [0],
+    "no-cond-assign": [0],
+    "import/no-unresolved": [0],
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "ignore"
+    }],
+    "object-curly-newline": [0],
+    "function-paren-newline": [0],
+    "no-restricted-globals": [0],
+    "require-yield": [1],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  }
+};
