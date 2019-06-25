95bc9275151d6fff919d8f2ea0cbb106b7123203
yuri2peter <yuri2peter@qq.com>
2019-06-25 21:42:15 +0800

    traditional

--- page.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  {
-    name: 'index',
-    path: 'home',
-  },
-];
--- src/pages/home/index.js
+++
@@ -1,7 +0,0 @@
-import readme from '../../../README.md';
-require('./index.scss');
-require('github-markdown-css');
-
-const md = require('markdown-it')();
-const html = md.render(readme);
-document.getElementById('readme').innerHTML = html;
